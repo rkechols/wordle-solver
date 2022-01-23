e431291d0160da24aeb0134c2fd58e6fccd98c94
Ryan Echols <rkechols21@gmail.com>
2022-01-23 14:27:43 -0700

    rename var

--- src/util/word_list.py
+++ src/util/word_list.py
@@ -15,18 +15,20 @@ WORD_LIST_FULL_PATH = get_word_list_path()
 
 def load_word_list(n_chars: int = 5) -> Set[str]:
     word_list_path = get_word_list_path(n_chars)
-    to_return = set()
+    words = set()
     if word_list_path.is_file():  # just load it
         with open(word_list_path, "r", encoding="utf-8") as f:
             for line in f:
-                to_return.add(line.strip().lower())
+                words.add(line.strip().lower())
     else:  # load the needed data and also create the file
         with open(WORD_LIST_FULL_PATH, "r", encoding="utf-8") as f:
             for line in f:
                 word = line.strip().lower()
                 if len(word) == n_chars:
-                    to_return.add(word)
+                    words.add(word)
+        if len(words) == 0:
+            raise ValueError(f"The full word list no words that are {n_chars} letters long.")
         with open(word_list_path, "w", encoding="utf-8") as f:
-            for word in sorted(to_return):
+            for word in sorted(words):
                 print(word, file=f)
-    return to_return
+    return words

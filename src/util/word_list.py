7abcbb970dd52ca8a07f78c276c000fa845be10e
Ryan Echols <rkechols21@gmail.com>
2022-01-23 13:21:07 -0700

    refactor WordList to just be a function

--- src/util/word_list.py
+++ src/util/word_list.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from typing import Set
 
 
 DATA_DIR = Path("data")
@@ -12,34 +13,20 @@ def get_word_list_path(n_chars: int = None) -> Path:
 WORD_LIST_FULL_PATH = get_word_list_path()
 
 
-class WordList:
-    def __init__(self, n_chars: int = 5):
-        self.n_chars = n_chars
-        word_list_path = get_word_list_path(self.n_chars)
-        self.data = set()
-        if word_list_path.is_file():  # just load it
-            with open(word_list_path, "r", encoding="utf-8") as f:
-                for line in f:
-                    self.data.add(line.strip().lower())
-        else:  # load the needed data and also create the file
-            with open(WORD_LIST_FULL_PATH, "r", encoding="utf-8") as f:
-                for line in f:
-                    word = line.strip().lower()
-                    if len(word) == self.n_chars:
-                        self.data.add(word)
-            with open(word_list_path, "w", encoding="utf-8") as f:
-                for word in sorted(self.data):
-                    print(word, file=f)
-
-    def __len__(self) -> int:
-        return len(self.data)
-
-    def __iter__(self):
-        for word in self.data:
-            yield word
-
-    def __repr__(self) -> str:
-        return f"{self.__class__.__name__}(n_chars={self.n_chars})"
-
-    def __str__(self) -> str:
-        return f"{self.__class__.__name__}(n_chars={self.n_chars}, size={len(self)})"
+def load_word_list(n_chars: int = 5) -> Set[str]:
+    word_list_path = get_word_list_path(n_chars)
+    to_return = set()
+    if word_list_path.is_file():  # just load it
+        with open(word_list_path, "r", encoding="utf-8") as f:
+            for line in f:
+                to_return.add(line.strip().lower())
+    else:  # load the needed data and also create the file
+        with open(WORD_LIST_FULL_PATH, "r", encoding="utf-8") as f:
+            for line in f:
+                word = line.strip().lower()
+                if len(word) == n_chars:
+                    to_return.add(word)
+        with open(word_list_path, "w", encoding="utf-8") as f:
+            for word in sorted(to_return):
+                print(word, file=f)
+    return to_return
